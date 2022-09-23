31e7df14ce1beb87856eb0f61070ad70d17814af
Dylan Juliano Santoso <djulianosantoso@gmail.com>
2022-09-23 13:28:51 +0700

    Initial Commit

---
+++ MacroChallengeScratch/MacroChallengeScratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroChallengeScratchApp.swift
+//  MacroChallengeScratch
+//
+//  Created by Dylan Juliano Santoso on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MacroChallengeScratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
